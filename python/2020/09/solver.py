a32ea591344fcbbba79e4370a307bdb8d1d935ce
cmoineau <moineaucyril@gmail.com>
2021-07-21 22:04:25 +0200

    Adding solution day 9 2020 python.

--- python/2020/09/solver.py
+++ python/2020/09/solver.py
@@ -1,40 +1,75 @@
-tab = []
-file = open("./input", "r")
+import argparse
 
-for line in file:
-    tab.append(int(line.strip()))
-file.close()
+parser = argparse.ArgumentParser(description='Advent of code solution.')
+parser.add_argument('--test','-t', default=False, action='store_true', help='Run the example')
+args = parser.parse_args()
 
+if args.test:
+    tab=[35,
+        20,
+        15,
+        25,
+        47,
+        40,
+        62,
+        55,
+        65,
+        95,
+        102,
+        117,
+        150,
+        182,
+        127,
+        219,
+        299,
+        277,
+        309,
+        576,]
+    key_size = 5
+else:
+    tab = []
+    file = open("./input", "r")
 
+    for line in file:
+        tab.append(int(line.strip()))
+    file.close()
+    key_size = 25
 
-# PART 1
-preamble = tab[:25]
-preamble.sort()
-datas = tab[25:]
+# Part 1
+for index in range(len(tab) - key_size-1):
+    keys = tab[index:index+key_size]
+    keys = sorted(keys)
+    if len(set(keys)) != key_size:
+        raise RuntimeError("Duplicate found in key !")
+    i = tab[index+key_size]
 
-def recombulator(number, tab=preamble):
-    min = 0
-    max = len(tab) -1
-    while tab[min] + tab[max] != number and min != max:
-        if tab[min] + tab[max] < number:
-            min += 1
-        else:
-            max -= 1
-    if tab[min] + tab[max] != number:
-        return False
+    if i > keys[-1] + keys[-2]:
+        print("Solution part 1 : ", i)
+        break
+    elif i < keys[0] + keys[1]:
+        print("Solution part 1 : ", i)
+        break
     else:
-        return True
+        bot = 0
+        top = key_size - 1
+        while keys[bot] + keys[top] != i:
+            if keys[bot] + keys[top] > i:
+                top -= 1
+            else:
+                bot +=1
+            if bot == top:
+                print("Solution part 1 : ", i)
+                break
 
-maximum = preamble[-1] + preamble[-2]
-minimum = preamble[0] + preamble[1]
-
-
-for data in datas:
-    if (not minimum<data<maximum):
-        print('0Answer :', data)
-        break
-    if recombulator(data):
-        print('1Answer :', data)
-        break
-        
-    
+# Part 2
+sum_elt = []
+index = 0
+min_index = 0
+while sum(sum_elt) != i:
+    sum_elt.append(tab[index])
+    index += 1
+    if sum(sum_elt) > i:
+        sum_elt = [sum_elt[1]]
+        min_index += 1
+        index = min_index +1
+print("Solution part 2 :", min(sum_elt) + max(sum_elt))
